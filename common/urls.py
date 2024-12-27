aec2e44d76b6022097c2effd5c163e91a8ffb0ec
SanjarbekAI <sanjarbekwork@gmail.com>
2024-12-27 17:46:19 +0500

    template managing

--- common/urls.py
+++ common/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from common.views import home_page_view, about_page_view
+from common.views import *
 
 app_name = 'pages'
 
 urlpatterns = [
     path('about/', about_page_view, name='about'),
+    path('contact/', contact_page_view, name='contact'),
+    path('submit/', submit_page_view, name='submit'),
+    path('recipies/', recipies_page_view, name='recipies'),
+    path('login/', login_page_view, name='login'),
+    path('category/', category_page_view, name='category'),
+    # path('shop/', about_page_view, name='about'),
     path('', home_page_view, name='home'),
 ]
